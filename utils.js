83112830c58f2810b9172e78564df67775f349c0
Jordan-Eckowitz <jordan.eckowitz@gmail.com>
2021-03-13 15:13:37 -0500

    started adding file to read named exports

--- utils.js
+++ utils.js
@@ -1,11 +1,34 @@
 // packages
-const { readdirSync, lstatSync } = require("fs");
+const { readdirSync, readFileSync, lstatSync } = require("fs");
+
+const namedExports = (content) => {
+  console.log(content);
+  const singleNamedExport = content.match(
+    /(?<=\b(export(\s*)(const|let|var)(\s*))|(exports.))(\w+)/g
+  );
+  const multiNamedExport = content.match(
+    /(?<=(module.exports(\s*)=(\s*){))(.*?)(?=})/gs
+  );
+  console.log("SINGLE", singleNamedExport);
+  console.log("MULTI", multiNamedExport);
+};
+
+const readFile = (filepath) => {
+  const content = readFileSync(filepath, "utf8");
+  namedExports(content);
+};
 
 module.exports.findFiles = (path) => {
   const dirItems = readdirSync(path);
   dirItems.forEach((dirItem) => {
-    const isFile = lstatSync(`${path}/${dirItem}`).isFile();
-    if (isFile) return console.log(`FILE - ${dirItem}`);
-    return console.log(`FOLDER - ${dirItem}`);
+    const dirItemPath = `${path}/${dirItem}`;
+    const isFile = lstatSync(dirItemPath).isFile();
+    if (isFile) {
+      // only ready *.ts, *.tsx, *.js, *.jsx files
+      if (dirItemPath.match(/ts|js/g)) {
+        return readFile(dirItemPath);
+      }
+    }
+    // return console.log(`FOLDER - ${dirItem}`);
   });
 };
