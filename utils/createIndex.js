04a67f9c112bd404d06b7ceb76a21d4ca43b9624
Jordan-Eckowitz <jordan.eckowitz@gmail.com>
2021-03-23 23:16:47 -0400

    added required exports

--- utils/createIndex.js
+++ utils/createIndex.js
@@ -2,20 +2,43 @@ const { relative } = require("path");
 
 const MAX_LINE_LENGTH = 80; // chars
 
-// export App, {Wrapper, Title} from "./..."
-const staticExport = (data, relativePath) => {
+// const App, {Wrapper, Title} = require("./...")
+// module.exports = {...}
+const requiredExport = (data, relativePath) => {
   console.log("DATA", data);
   const anonymousOrDefault = [...data.anonymous, ...data.default];
-  // if (anonymousOrDefault.length > 1) {
-  //   return console.log("ERROR - ONLY ALLOWED ONE ANONYMOUSE/DEFAULT EXPORT PER FILE");
-  // }
+  const anonymousOrDefaultValue = anonymousOrDefault[0] || "";
+  let exportStart = `const ${anonymousOrDefaultValue}`;
+  const fromPath = ` = require("${relativePath}")`;
+
+  let middleExport = "";
+  if (data.named.length > 0) {
+    if (anonymousOrDefaultValue.length > 0) {
+      exportStart += ", ";
+    }
+    middleExport = `{ ${data.named.join(", ")} }`;
+    if ((exportStart + middleExport + fromPath).length > MAX_LINE_LENGTH) {
+      middleExport = `{${data.named.map((name) => `\n\t${name}`).join(",")}\n}`;
+    }
+  }
+  return {
+    imports: exportStart + middleExport + fromPath,
+    exports: [...anonymousOrDefault, ...data.named],
+  };
+};
+
+// export App, {Wrapper, Title} from "./..."
+const staticExport = (data, relativePath) => {
+  const anonymousOrDefault = [...data.anonymous, ...data.default];
   const anonymousOrDefaultValue = anonymousOrDefault[0] || "";
   let exportStart = `export ${anonymousOrDefaultValue}`;
   const fromPath = ` from "${relativePath}"`;
 
   let middleExport = "";
   if (data.named.length > 0) {
-    if (anonymousOrDefaultValue.length > 0) exportStart += ", ";
+    if (anonymousOrDefaultValue.length > 0) {
+      exportStart += ", ";
+    }
     middleExport = `{ ${data.named.join(", ")} }`;
     if ((exportStart + middleExport + fromPath).length > MAX_LINE_LENGTH) {
       middleExport = `{${data.named.map((name) => `\n\t${name}`).join(",")}\n}`;
@@ -28,23 +51,7 @@ module.exports.createIndex = (path, data) => {
   const exports = data.map(({ filepath, static, required, types }) => {
     const relativePath = `./${relative(path, filepath)}`;
     const staticFileExport = staticExport(static, relativePath);
-    console.log("STATIC", staticFileExport);
+    const requiredFileExport = requiredExport(required, relativePath);
+    console.log(requiredFileExport);
   });
-  // TODO: required exports
-  // TODO: type exports
 };
-
-// return {
-//   filepath,
-//   static: {
-//     named: staticNamedExports,
-//     anonymous: staticAnonymousExports,
-//     default: staticDefaultExports,
-//   },
-//   required: {
-//     named: requiredNamedExports,
-//     anonymous: requiredAnonymousExports,
-//     default: requiredDefaultExports,
-//   },
-//   types: allTypeExports,
-// }
