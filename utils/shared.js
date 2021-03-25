6b06eb69bcab8a93e1b6c2b0ec6bfa99d4ebffb1
Jordan-Eckowitz <jordan.eckowitz@gmail.com>
2021-03-24 22:48:55 -0400

    if file name is index then use the name of the parent directory

--- utils/shared.js
+++ utils/shared.js
@@ -1,3 +1,8 @@
 module.exports.getFilename = (filepath) => {
-  return filepath.split("/").slice(-1)[0].split(".")[0];
+  const fileName = filepath.split("/").slice(-1)[0].split(".")[0];
+  // if file name is index then use the name of the parent directory
+  if (fileName === "index") {
+    return filepath.split("/").slice(-2)[0];
+  }
+  return fileName;
 };
