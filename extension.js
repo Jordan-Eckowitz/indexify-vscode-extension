9d836910f92c5e40963af489882273dd3623e227
Jordan-Eckowitz <jordan.eckowitz@gmail.com>
2021-03-26 16:24:47 -0400

    split indexify into 2 commands - one for deep index and another for shallow

--- extension.js
+++ extension.js
@@ -4,6 +4,22 @@ const vscode = require("vscode");
 // utils
 const { getExports, createIndex, formatExclusions } = require("./utils");
 
+const indexify = (path, includeNestedDirectories) => {
+  const config = vscode.workspace.getConfiguration("indexify");
+  const exclusions = formatExclusions(config.get("exclude.directoryList"));
+  const includeIndexFiles = config.get("include.otherIndexFiles");
+
+  const exports = getExports(path, exclusions, includeNestedDirectories);
+  createIndex(path, exports, includeIndexFiles);
+
+  const rootDir = path.split("/").slice(-1);
+
+  // Display a message box to the user
+  vscode.window.showInformationMessage(
+    `index file created in "${rootDir}" directory`
+  );
+};
+
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 
@@ -18,23 +34,18 @@ function activate(context) {
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with  registerCommand
   // The commandId parameter must match the command field in package.json
-  let disposable = vscode.commands.registerCommand(
-    "indexify.contextMenu",
-    ({ path }) => {
-      const config = vscode.workspace.getConfiguration("indexify");
-      const exclusions = formatExclusions(config.get("exclude.directoryList"));
-      const includeIndexFiles = config.get("include.otherIndexFiles");
-      const includeNestedDirectories = config.get("include.nestedDirectories");
-
-      const exports = getExports(path, exclusions, includeNestedDirectories);
-      createIndex(path, exports, includeIndexFiles);
-
-      // Display a message box to the user
-      vscode.window.showInformationMessage(`index file created in "${path}"`);
-    }
+  const indexifyDeep = vscode.commands.registerCommand(
+    "indexify.contextMenu.deep",
+    ({ path }) => indexify(path, true)
   );
 
-  context.subscriptions.push(disposable);
+  const indexifyShallow = vscode.commands.registerCommand(
+    "indexify.contextMenu.shallow",
+    ({ path }) => indexify(path, false)
+  );
+
+  context.subscriptions.push(indexifyDeep);
+  context.subscriptions.push(indexifyShallow);
 }
 
 // this method is called when your extension is deactivated
